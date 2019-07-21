9c46fdec66cd76a8ec24a47e8cf634537c13cd06
Martin Polder <martinpolder@gmail.com>
2019-07-21 10:35:37 -0400

    linked everything: firebase, scripts, styling

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyA2v6r2k1Gkv8FOuFdM9dFwrAqX8BE4TQA",
+    authDomain: "train-times-dfd4e.firebaseapp.com",
+    databaseURL: "https://train-times-dfd4e.firebaseio.com",
+    projectId: "train-times-dfd4e",
+    storageBucket: "",
+    messagingSenderId: "919363791404",
+    appId: "1:919363791404:web:2c73346e77894c3b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
